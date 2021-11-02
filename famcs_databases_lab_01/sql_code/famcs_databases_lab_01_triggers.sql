8c99fd2a5cab8258553184052f0723ba9c6ecfd0
Veta <veta.s.working@gmail.com>
2021-11-02 14:54:40 +0300

    Update famcs_databases_lab_01_triggers.sql

--- famcs_databases_lab_01/sql_code/famcs_databases_lab_01_triggers.sql
+++ famcs_databases_lab_01/sql_code/famcs_databases_lab_01_triggers.sql
@@ -8,10 +8,12 @@ BEGIN
     SELECT COUNT(*) into number_of_rents FROM rent_contracts
         WHERE car_id = :new.car_id AND ((:new.rent_starting_date BETWEEN rent_starting_date AND rent_ending_date)
             OR (:new.rent_ending_date BETWEEN rent_starting_date AND rent_ending_date)
-            OR (rent_ending_date BETWEEN :new.rent_starting_date AND :new.rent_ending_date)
+            OR (rent_starting_date BETWEEN :new.rent_starting_date AND :new.rent_ending_date)
             OR (rent_ending_date BETWEEN :new.rent_starting_date AND :new.rent_ending_date)
             OR (rent_starting_date = :new.rent_starting_date)
-            OR (rent_ending_date = :new.rent_ending_date));
+            OR (rent_starting_date = :new.rent_ending_date)
+            OR (rent_ending_date = :new.rent_ending_date)
+            OR (rent_ending_date = :new.rent_starting_date));
     IF number_of_rents > 1
     THEN RAISE_APPLICATION_ERROR(-20001, 'Временные интервалы сдачи автомобиля не должны пересекаться !');
     END IF;
@@ -27,12 +29,14 @@ BEGIN
     SELECT COUNT(*) into number_of_cars FROM rent_contracts
         WHERE renter_id = :new.renter_id AND ((:new.rent_starting_date BETWEEN rent_starting_date AND rent_ending_date)
             OR (:new.rent_ending_date BETWEEN rent_starting_date AND rent_ending_date)
-            OR (rent_ending_date BETWEEN :new.rent_starting_date AND :new.rent_ending_date)
+            OR (rent_starting_date BETWEEN :new.rent_starting_date AND :new.rent_ending_date)
             OR (rent_ending_date BETWEEN :new.rent_starting_date AND :new.rent_ending_date)
             OR (rent_starting_date = :new.rent_starting_date)
-            OR (rent_ending_date = :new.rent_ending_date));
+            OR (rent_starting_date = :new.rent_ending_date)
+            OR (rent_ending_date = :new.rent_ending_date)
+            OR (rent_ending_date = :new.rent_starting_date));
     IF number_of_cars >= 2
-    THEN RAISE_APPLICATION_ERROR(-20001, 'В аренде у одного клиента не может находиться более 2-х автомобилей одновременно !');
+    THEN RAISE_APPLICATION_ERROR(-20002, 'В аренде у одного клиента не может находиться более 2-х автомобилей одновременно !');
     END IF;
 END;
 /
@@ -42,6 +46,6 @@ BEFORE INSERT OR UPDATE ON rent_contracts
 FOR EACH ROW
 BEGIN
   IF (:new.final_cost < 100)
-  THEN RAISE_APPLICATION_ERROR(-20998, 'Окончательная стоимость аренды составляет менее 100 рублей !');
+  THEN RAISE_APPLICATION_ERROR(-20003, 'Окончательная стоимость аренды составляет менее 100 рублей !');
   END IF;
 END;
