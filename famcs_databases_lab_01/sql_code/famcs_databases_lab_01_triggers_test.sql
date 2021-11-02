824238de6aa31ae89970d4e3d75c986fa4858e6c
Veta <veta.s.working@gmail.com>
2021-11-02 14:45:44 +0300

    Update famcs_databases_lab_01_triggers_test.sql

--- famcs_databases_lab_01/sql_code/famcs_databases_lab_01_triggers_test.sql
+++ famcs_databases_lab_01/sql_code/famcs_databases_lab_01_triggers_test.sql
@@ -1,7 +1,7 @@
 -- триггер должен отслеживать, чтобы временные интервалы сдачи автомобиля не пересекались;
-INSERT INTO rent_contracts VALUES (14, 2, 1, to_date('2021-11-02', 'yyyy-mm-dd'), to_date('2021-11-08', 'yyyy-mm-dd'), NULL, NULL, NULL, 100, NULL, 100);
-INSERT INTO rent_contracts VALUES (15, 4, 1, to_date('2021-11-04', 'yyyy-mm-dd'), to_date('2021-11-08', 'yyyy-mm-dd'), NULL, NULL, NULL, 100, NULL, 100);
-INSERT INTO rent_contracts VALUES (16, 4, 1, to_date('2021-11-02', 'yyyy-mm-dd'), to_date('2021-11-04', 'yyyy-mm-dd'), NULL, NULL, NULL, 100, NULL, 100);
+INSERT INTO rent_contracts VALUES (14, 1, 1, to_date('2021-11-02', 'yyyy-mm-dd'), to_date('2021-11-08', 'yyyy-mm-dd'), NULL, NULL, NULL, 100, NULL, 100);
+INSERT INTO rent_contracts VALUES (15, 2, 1, to_date('2021-11-04', 'yyyy-mm-dd'), to_date('2021-11-08', 'yyyy-mm-dd'), NULL, NULL, NULL, 100, NULL, 100);
+INSERT INTO rent_contracts VALUES (16, 3, 1, to_date('2021-11-02', 'yyyy-mm-dd'), to_date('2021-11-04', 'yyyy-mm-dd'), NULL, NULL, NULL, 100, NULL, 100);
 
 -- триггер должен отслеживать, чтобы в аренде у одного клиента не находилось более 2-х автомобилей одновременно;
 INSERT INTO rent_contracts VALUES (17, 4, 1, to_date('2021-11-09', 'yyyy-mm-dd'), to_date('2021-11-18', 'yyyy-mm-dd'), NULL, NULL, NULL, 100, NULL, 100);
